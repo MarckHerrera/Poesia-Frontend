6a073aa4767edd2dd9c5b3b83b370e3041ba8bd6
mherrera 2020235 <88869102+MarckHerrera@users.noreply.github.com>
2022-08-24 00:02:34 -0600

    Pre Heroku

--- poesia-front/src/components/Main/Main.jsx
+++ poesia-front/src/components/Main/Main.jsx
@@ -4,7 +4,9 @@ import { Routes, Route, Navigate, } from "react-router-dom";
 import { ThemeProvider } from '@mui/material/styles';
 import { Theme } from '../../Theme/Theme';
 import Formulario from '../Formulario/Formulario';
-
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import Reporte from '../Reporte/Reporte';
 
 
 
@@ -12,19 +14,27 @@ const Main = () => {
 
     return (
         <ThemeProvider theme={Theme}>
-            <>
-                <Routes>
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                    <Route path="/reporte" element={<Navigate to="/" replace />} />
-                    <Route path="/" element={
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+
+                    <Routes>
+                        <Route path="*" element={<Navigate to="/" replace />} />
+                        <Route path="/" element={
 
 
                             <Formulario
                             />
 
-                    } />
-                </Routes>
-            </>
+                        } />
+                        <Route path="/reporte" element={
+
+
+                            <Reporte
+                            />
+
+                        } />
+                    </Routes>
+
+            </LocalizationProvider>
         </ThemeProvider>
     )
 }
